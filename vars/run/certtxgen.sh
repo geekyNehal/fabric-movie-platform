2f2c5904d7ddc632c7d594cdee18871bbee084ad
geekyNehal <nehaljohn91@gmail.com>
2023-04-27 19:47:18 +0530

    removed unwanted files

--- vars/run/certtxgen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd $FABRIC_CFG_PATH
-# cryptogen generate --config crypto-config.yaml --output keyfiles
-configtxgen -profile OrdererGenesis -outputBlock genesis.block -channelID systemchannel
-
-configtxgen -printOrg cbfc-movie-com > JoinRequest_cbfc-movie-com.json
-configtxgen -printOrg inox-movie-com > JoinRequest_inox-movie-com.json
-configtxgen -printOrg pvr-movie-com > JoinRequest_pvr-movie-com.json
-configtxgen -printOrg tokenplatform-movie-com > JoinRequest_tokenplatform-movie-com.json
